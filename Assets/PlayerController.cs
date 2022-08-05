67b987c8eb13eb58070803962df6fa65fa5b6b9e
ppaka <asj0216@gmail.com>
2022-08-05 21:55:55 +0900

    2022-08-05

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Transform camTf;
-    
+    public float speed = 8;
+
     private void Update()
     {
         transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, camTf.rotation.eulerAngles.y,
             camTf.rotation.eulerAngles.z));
+        
+        var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.Translate(input.normalized * (Time.deltaTime * speed));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            print("적한테 부딫혔음");
+        }
     }
 }
