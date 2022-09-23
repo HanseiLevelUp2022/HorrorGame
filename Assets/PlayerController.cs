79b0262ad86b83d1d4b0291e3ffcfa3ffd6b647c
ppaka <asj0216@gmail.com>
2022-09-23 22:04:44 +0900

    2022-09-23

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,21 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform camTf;
-    public float speed = 8;
-
-    private void Update()
-    {
-        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, camTf.rotation.eulerAngles.y,
-            camTf.rotation.eulerAngles.z));
-        
-        var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        transform.Translate(input.normalized * (Time.deltaTime * speed));
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy"))
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
